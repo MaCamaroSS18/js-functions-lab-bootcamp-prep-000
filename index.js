8e7cfebe1f90db0a6574bdbb038d769c73591b20
Dominique McLemore <macmanco85@gmail.com>
2019-07-16 04:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidayTo(holiday){
   return 'Happy Independence Day, you!'
 }
 function holidayCountdown(holiday, days){
-  return 'It\'s 20 days until Mothers\'s Day!'
+  return 'It\'s 20 days until Mother\'s Day!'
 }
