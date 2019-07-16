f2f4fd7f02e04fee85f1db68fd7ba5c8360cb293
Dominique McLemore <macmanco85@gmail.com>
2019-07-16 04:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function happyHolidays(string){
   return 'Happy holidays!'
 }
-var name = 'you'
 function happyHolidaysTo(name){
-  'Happy holidays, ${name}!'
+  return 'Happy holidays, ${name}!'
 }
